5033cf07bd9f065320141eb22f94df7e3a3af3e9
dasWarder <Al.94_94@mail.ru>
2021-05-27 13:54:48 +0300

    Change properties to yaml, delete quartz and add EmailService

--- src/main/java/com/example/emailscheduler/EmailSchedulerApplication.java
+++ src/main/java/com/example/emailscheduler/EmailSchedulerApplication.java
@@ -1,13 +1,25 @@
 package com.example.emailscheduler;
 
+import com.example.emailscheduler.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class EmailSchedulerApplication {
 
+    @Autowired
+    private EmailService emailService;
+
     public static void main(String[] args) {
         SpringApplication.run(EmailSchedulerApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void trigger() {
+        emailService.sendEmail("babichev@tlu.ee", "This is just a test message", "Test");
     }
 
 }
